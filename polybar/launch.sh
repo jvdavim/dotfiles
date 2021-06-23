fba44965369d13f00e04e9d2c12c033e9946e495
Joao Victor Davim <joaovictor_rj@poli.ufrj.br>
2021-06-23 09:23:32 -0300

    fix multi monitor polybar and i3wm module loop scroll

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,17 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
-if [ ${#outputs[@]} < 2 ]; then
-  for m in $outputs; do
-    MONITOR=$m polybar --reload default &
-  done
-else
-  for m in $outputs; do
-    if [ $m == 'eDP1' ]; then
-      MONITOR=$m polybar --reload extension &
-    elif [ $m == 'HDMI1' ]; then
-      MONITOR=$m polybar --reload default &
-    fi
-  done
-fi
+m=$(xrandr --query | grep " connected" | grep " primary" | cut -d" " -f1)
+MONITOR=$m polybar --reload default &
+
+m=$(xrandr --query | grep " connected" | grep -v " primary" | cut -d" " -f1)
+MONITOR=$m polybar --reload extension &
